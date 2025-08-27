72cb320f75d001b31340c0a028dc1e20c6cd7415
PRAVEENKUMAR K <praveenkum1225@gmail.com>
2025-08-28 01:44:37 +0530

    second commit

--- springsec/src/main/java/com/jimmy/springsec/config/SpringSecurityConfig.java
+++ springsec/src/main/java/com/jimmy/springsec/config/SpringSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.jimmy.springsec.config;
 
+import com.jimmy.springsec.security.BankAccessDeniedHandler;
+import com.jimmy.springsec.security.BankAuthenticationEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -11,10 +13,13 @@ public class SpringSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((request)->
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(request->
                 request.anyRequest().permitAll())
-                .httpBasic(Customizer.withDefaults())
-                .formLogin(Customizer.withDefaults());
+                .httpBasic(hbc -> hbc.authenticationEntryPoint(new BankAuthenticationEntryPoint()))
+                .formLogin(flc -> flc.disable())
+                .exceptionHandling(ehc -> ehc.accessDeniedHandler(new BankAccessDeniedHandler()));
 
         return http.build();
     }
